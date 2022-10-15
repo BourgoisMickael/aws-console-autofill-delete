37c49d48c32d36b64b8e37ac13afb119c7f27c75
BourgoisMickael <mickael.bourgois@epitech.eu>
2022-10-15 11:27:32 +0200

    Setup cypress with extension

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        // supply the absolute path to an unpacked extension's folder
+        // NOTE: extensions cannot be loaded in headless Chrome
+        launchOptions.extensions.push(`${__dirname}/src`)
+      
+        return launchOptions
+      })
+    },
+  },
+});
