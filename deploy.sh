d8391d5669a9d463418f04c5d99395693061174a
BourgoisMickael <mickael.bourgois@epitech.eu>
2022-10-26 21:28:47 +0100

    Create a test stack with testing resources

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sam deploy \
+    --region eu-west-3 \
+    --template-file test-stack.yml \
+    --no-fail-on-empty-changeset \
+    --stack-name autofill-delete-test-stack \
+    --capabilities CAPABILITY_NAMED_IAM \
