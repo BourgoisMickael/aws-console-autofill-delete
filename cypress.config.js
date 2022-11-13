b191c3003f8e61f8faa589982cb20ecc7ceefab4
BourgoisMickael <mickael.bourgois@epitech.eu>
2022-11-13 21:58:57 +0100

    Add configuration for firefox addon

--- cypress.config.js
+++ cypress.config.js
@@ -5,12 +5,15 @@ module.exports = defineConfig({
   e2e: {
     defaultCommandTimeout: 8000,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       on('before:browser:launch', (browser, launchOptions) => {
-        // supply the absolute path to an unpacked extension's folder
-        // NOTE: extensions cannot be loaded in headless Chrome
-        launchOptions.extensions.push(path.join(__dirname, 'src'));
-        launchOptions.args.push('--auto-open-devtools-for-tabs'); // chrome
+        if (browser.family === 'firefox') {
+          launchOptions.extensions.push(path.join(__dirname, 'build/firefox'));
+          launchOptions.args.push('--devtools');
+        }
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          launchOptions.extensions.push(path.join(__dirname, 'build/chrome'));
+          launchOptions.args.push('--auto-open-devtools-for-tabs');
+        }
 
         return launchOptions;
       });
